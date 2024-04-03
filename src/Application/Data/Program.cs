8447a199d5ce2bd772bd6a787b7a5f13e2f6ae29
Olenka Stoliar <olenka04st@gmail.com>
2024-04-03 09:52:29 +0300

    Fix build

--- src/Application/Data/Program.cs
+++ src/Application/Data/Program.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 Console.WriteLine("Hello, World!");
 
 var optionsBuilder = new DbContextOptionsBuilder<PostgresContext>();
-optionsBuilder.UseNpgsql("<connectionString>");
+optionsBuilder.UseNpgsql("Server=;Port=;Database=BookSwapHub;Username=;Password=");
 var context = new PostgresContext(optionsBuilder.Options);
 
 var canConnect = context.Database.CanConnect();
--- src/Application/Web/Program.cs
+++ src/Application/Web/Program.cs
@@ -1,17 +1,17 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Web.Data;
+using Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
+builder.Services.AddDbContext<PostgresContext>(options =>
     options.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+    .AddEntityFrameworkStores<PostgresContext>();
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
